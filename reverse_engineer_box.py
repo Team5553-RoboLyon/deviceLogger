2ee92cd5e1c7a2ba40f71fca3719b1d8ea5560e2
Oxheracy <40737205+Oxheracy@users.noreply.github.com>
2022-02-05 12:10:37 +0100

    Update reverse_engineer_box.py

--- reverse_engineer_box.py
+++ reverse_engineer_box.py
@@ -4,11 +4,13 @@
 import requests
 import json
 from datetime import datetime
-import sys
+import pytz
 from prettytable import PrettyTable
 
+prefix = "3e673c0f"
+
 cookies = {
-    '45b4c072/accept-language': 'fr,fr-FR',
+    prefix + '/accept-language': 'fr,fr-FR',
     'UILang': 'fr',
 }
 headers = {
@@ -17,25 +19,25 @@ headers = {
     'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
     'Authorization': 'X-Sah-Login',
     'Content-Type': 'application/x-sah-ws-4-call+json',
-    'Origin': 'https://nas.robolyon.com:14454',
+    'Origin': 'https://192.168.1.1',
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Referer': 'https://nas.robolyon.com:14454/homeAuthentificationRemote.html',
+    'Referer': 'https://192.168.1.1/homeAuthentificationRemote.html',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 }
 data = '{"service":"sah.Device.Information","method":"createContext","parameters":{"applicationName":"webui","username":"adminremote","password":"r5HWGRro"}}'
-loginResponse = requests.post('https://nas.robolyon.com:14454/ws', headers=headers, cookies=cookies, data=data, verify = False)
+loginResponse = requests.post('https://192.168.1.1/ws', headers=headers, cookies=cookies, data=data, verify = False)
 
 
 # loginResponse.cookies.get_dict()['45b4c072/sessid']
 # json.loads(loginResponse.text)['data']['contextID']
 
 cookies = {
-    '45b4c072/accept-language': 'fr,fr-FR',
+    prefix + '/accept-language': 'fr,fr-FR',
     'UILang': 'fr',
-    '45b4c072/sessid': loginResponse.cookies.get_dict()['45b4c072/sessid'],
+    prefix + '/sessid': loginResponse.cookies.get_dict()[prefix + '/sessid'],
     'sah/contextId': json.loads(loginResponse.text)['data']['contextID'],
 }
 headers = {
@@ -46,16 +48,16 @@ headers = {
     'Content-Type': 'application/x-sah-ws-4-call+json',
     'Authorization': 'X-Sah ' +json.loads(loginResponse.text)['data']['contextID'],
     'X-Context': json.loads(loginResponse.text)['data']['contextID'],
-    'Origin': 'https://nas.robolyon.com:14454',
+    'Origin': 'https://192.168.1.1',
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Referer': 'https://nas.robolyon.com:14454/homeAuthentificationRemote.html',
+    'Referer': 'https://192.168.1.1/homeAuthentificationRemote.html',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 }
 data = '{"service":"TopologyDiagnostics","method":"buildTopology","parameters":{"SendXmlFile":false}}'
-dataResponse = requests.post('https://nas.robolyon.com:14454/ws', headers=headers, cookies=cookies, data=data, verify=False)
+dataResponse = requests.post('https://192.168.1.1/ws', headers=headers, cookies=cookies, data=data, verify=False)
 
 
 t = PrettyTable(['A', 'B', 'C', "D"])
@@ -64,7 +66,7 @@ for i in json.loads(dataResponse.text)['status'][0]['Children'][0]['Children']:
     if 'Children' in i:
         for j in i['Children']:
             if j['Active']==True and 'IPAddress' in j:
-                current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                current_time = datetime.now().astimezone(pytz.timezone("Europe/Paris")).strftime('%Y-%m-%d %H:%M:%S')
                 t.add_row([current_time,j['Name'],j['Key'],j['IPAddress']])
 
 t.header=False
@@ -73,5 +75,3 @@ t.border=False
 with open("log.txt", "a") as file:
     file.write(str(t))
     file.write("\n\n")
-
-
